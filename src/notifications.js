1805ff07d7c12d2e9d27ba6d8c1a23b81a03fc8a
Minakoss <nontaspapastefanis@gmail.com>
2026-09-07 23:41:31 +0300

    update 22

--- src/notifications.js
+++ src/notifications.js
@@ -13,11 +13,10 @@ function urlBase64ToUint8Array(base64String) {
 
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
 
-  const rawData = window.atob(base64String);
+  const rawData = window.atob(base64);
 
   return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
 }
-
 /* ---------------------------------------------------------
    REGISTER SERVICE WORKER
 --------------------------------------------------------- */
